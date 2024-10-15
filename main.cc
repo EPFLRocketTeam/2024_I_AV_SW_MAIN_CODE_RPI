8ec33b556a50f1af7dd783401a88f6114f3ff86e
DAFER <mohamed.dafer@epfl.ch>
2024-10-15 16:31:06 +0200

    comment code and check task.cc that variables have the desired properties

--- main.cc
+++ main.cc
@@ -2,29 +2,45 @@
 #include <iostream>
 using namespace std;
 
+// Task function for task 1.
 void *task_function_1()
 {
     cout << "Hello from task 1!" << endl;
     return nullptr;
 }
-
+// Task function for task 2.
 void *task_function_2()
 {
     cout << "Hello from task 2!" << endl;
     return nullptr;
 }
+void *task_function_3()
+{
+    cout << "Hello from task 3!" << endl;
+    return nullptr;
+}
+void *task_function_4()
+{
+    cout << "Hello from task 4!" << endl;
+    return nullptr;
+}
 
 int main()
 {
+    // Initialize tasks with name, period, priority, CPU core, and function.
     Task tasks[] = {
-        Task("task1", 100e6, 90, 0, task_function_1),
-        Task("task2", 50e6, 80, 3, task_function_2)
+        Task("task1", 100e6, 80, 0, task_function_1),
+        Task("task2", 50e6, 80, 1, task_function_2),
+        Task("task3", 150e6, 80, 2, task_function_3),
+        Task("task4", 200e6, 80, 3, task_function_4)
     };
 
+    // Start all tasks.
     for (auto &task : tasks) {
         task.start_task();
     }
 
+    // Wait for all tasks to complete.
     for (auto &task : tasks) {
         task.wait_for_task();
     }
--- task.h
+++ task.h
@@ -5,29 +5,45 @@
 #include <pthread.h>
 #include <string>
 
+// The Task class represents a periodic task that can be run on a specific CPU core with a given priority.
 class Task
 {
 public:
+    // Constructor to initialize the task with a name, period in nanoseconds, priority, CPU core, and the task function.
     Task(std::string name, long int period_ns, int priority, int cpu_core, void *(*task_functions)(void));
+
+    // Starts the task by creating a thread and running the task loop.
     int start_task();
+
+    // Waits for the task to complete.
     int wait_for_task();
-    // TODO: destructor
+
+    // TODO: Add a destructor to clean up resources.
 
 private:
-    std::string name;
-    long int period_ns;
-    int priority;
-    int cpu_core;
-    void *(*task_function)();
+    std::string name; // Name of the task.
+    long int period_ns; // Period of the task in nanoseconds.
+    int priority; // Priority of the task.
+    int cpu_core; // CPU core on which the task should run.
+    void *(*task_function)(); // Function pointer to the task function.
 
-    pthread_attr_t thread_attributes;
-    pthread_t thread;
-    struct timespec next_period;
+    pthread_attr_t thread_attributes; // Thread attributes for the task.
+    pthread_t thread; // Thread handle for the task.
+    struct timespec next_period; // Time for the next period of the task.
 
+    // Prepares the task by setting thread attributes and other necessary configurations.
     int prepare_task();
+
+    // Static wrapper function to call the task loop.
     static void *task_loop_wrapper(void *context);
+
+    // The main task loop that runs the task periodically.
     void *task_loop();
+
+    // Measures the time and runs the task function.
     void measure_and_run_task();
+
+    // Increments the period to the next cycle.
     void increment_period();
 };
 
